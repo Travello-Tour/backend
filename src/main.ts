a3dc13fc16d08cd405c74ab18ce96c142193469d
Вячеслав Воробьев <slavavorobey@vk.com>
2024-05-27 15:51:33 +0300

    [add swager]

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
@@ -10,6 +10,15 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Accept',
     credentials: true,
   })
-  await app.listen(8080);
+  const config = new DocumentBuilder()
+    .setTitle('API для туров')
+    .setDescription('API description and examples')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
--- src/tour/tour.controller.ts
+++ src/tour/tour.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { TourService } from './tour.service';
+@ApiTags('API')
 @Controller('tour')
 export class TourController {
   constructor(private readonly tourService: TourService) {}
 
   @Get(':id')
+  @ApiOperation({ summary: 'Получить информация о туре' })
+  @ApiResponse({ status: 200, description: 'Информация о туре' })
   getTourById(@Param('id') id: number) {
     return this.tourService.getApiData(id);
   }
