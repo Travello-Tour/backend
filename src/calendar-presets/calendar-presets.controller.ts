f5a99a04643e5cad1c755405107981ed5b523693
Вячеслав Воробьев <slavavorobey@vk.com>
2024-05-31 00:10:27 +0300

    add descr swagger

--- src/calendar-presets/calendar-presets.controller.ts
+++ src/calendar-presets/calendar-presets.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CalendarPresetsService } from './calendar-presets.service';
 
 @ApiTags('API')
@@ -10,6 +10,7 @@ export class CalendarPresetsController {
   ) {}
 
   @Get()
+  @ApiOperation({ summary: 'Календарный пресет' })
   getData() {
     return this.calendarPresetsService.getApiData();
   }
